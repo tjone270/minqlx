907cb7fed14db7a51a49e77a587915a329b0b4e4
Mino <mino@minomino.org>
2015-11-16 13:01:32 +0100

    Make the owner be a level 5 at a database level for minqlx.database.Redis. This fixes a bug where you could ban/silence yourself.

--- python/minqlx/database.py
+++ python/minqlx/database.py
@@ -138,15 +138,20 @@ class Redis(AbstractDatabase):
         """Gets the permission of a player.
 
         :param player: The player in question.
-        :type player: minqlx.Player
+        :type player: minqlx.Player, int
         :returns: int
 
         """
         if isinstance(player, minqlx.Player):
-            key = "minqlx:players:{}:permission".format(player.steam_id)
+            steam_id = player.steam_id
         else:
-            key = "minqlx:players:{}:permission".format(player)
+            steam_id = steam_id
+
+        # If it's the owner, treat it like a 5.
+        if steam_id == minqlx.owner():
+            return 5
         
+        key = "minqlx:players:{}:permission".format(steam_id)
         perm = self[key]
         if perm == None:
             return 0
