5d2e586d0cc0553a2ad148b7719aa1ca0783f72f
Mino <mino@minomino.org>
2015-12-04 07:00:11 +0100

    Added minqlx.redirect_print(), which can redirect Com_Printf() output to a channel.

--- python/minqlx/_handlers.py
+++ python/minqlx/_handlers.py
@@ -389,10 +389,54 @@ def handle_console_print(text):
         if res == False:
             return False
         elif isinstance(res, str):
-            return res
+            text = res
+
+        if _print_redirection:
+            global _print_buffer
+            _print_buffer += text
+
+        return res
     except:
         minqlx.log_exception()
         return True
+
+_print_redirection = None
+_print_buffer = ""
+
+def redirect_print(channel):
+    """Redirects print output to a channel. Useful for commands that execute console commands
+    and want to redirect the output to the channel instead of letting it go to the console.
+
+    To use it, use the return value with the "with" statement.
+
+    .. code-block:: python
+        def cmd_echo(self, player, msg, channel):
+            with minqlx.redirect_print(channel):
+                minqlx.console_command("echo {}".format(" ".join(msg)))
+
+    """
+    class PrintRedirector:
+        def __init__(self, channel):
+            if not isinstance(channel, minqlx.AbstractChannel):
+                raise ValueError("The redirection channel must be an instance of minqlx.AbstractChannel.")
+            
+            self.channel = channel
+
+        def __enter__(self):
+            global _print_redirection
+            _print_redirection = self.channel
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            global _print_redirection
+            self.flush()
+            _print_redirection = None
+
+        def flush(self):
+            global _print_buffer
+            self.channel.reply(_print_buffer)
+            _print_buffer = ""
+
+    return PrintRedirector(channel)
 
 def register_handlers():
     minqlx.register_handler("rcon", handle_rcon)
