36edeab6034058c22fbc7f910c0c8b67df19ba69
Eugene Molotov <em92@users.noreply.github.com>
2019-09-19 05:57:25 +0000

    Fixed typo

--- python/minqlx/_game.py
+++ python/minqlx/_game.py
@@ -86,7 +86,7 @@ class Game():
 
     @property
     def is_team_game(self):
-        return 2 < int(self["g_gametype"] < 12
+        return 2 < int(self["g_gametype"]) < 12
 
     @property
     def map(self):
